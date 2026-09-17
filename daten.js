860d0442d9a3696d7875e6e76a5b64132d4a1c94
sndjan <sander.jan@gmx.net>
2026-09-17 19:34:33 +0200

    Add new weights and measurements

--- daten.js
+++ daten.js
@@ -14,11 +14,17 @@ window.__WEIGHT_DATA__ = {
         { "datum": "2026-07-26", "gewicht": 113.9 },
         { "datum": "2026-08-03", "gewicht": 114.6 },
         { "datum": "2026-08-09", "gewicht": 113.8 },
-        { "datum": "2026-08-18", "gewicht": 113.4, "kfa": 31.9, "muskelmasse": 73.37, "ffm": 77.23 }
+        { "datum": "2026-08-18", "gewicht": 113.4, "kfa": 31.9, "muskelmasse": 73.37, "ffm": 77.23 },
+        { "datum": "2026-08-23", "gewicht": 114.5 },
+        { "datum": "2026-08-30", "gewicht": 113.5 },
+        { "datum": "2026-09-06", "gewicht": 112.3 },
+        { "datum": "2026-08-13", "gewicht": 112.0 },
+        { "datum": "2026-09-17", "gewicht": 112.2, "kfa": 31.6, "muskelmasse": 72.93, "ffm": 76.74 }
       ],
       "umfaenge": [
         { "datum": "2026-07-13", "brust": 115.0, "schulter": 129.6, "bauch": 112.8, "taille": 107.4, "oberschenkelLinks": 69.4, "oberschenkelRechts": 67.6, "wadeLinks": 45.8, "wadeRechts": 44.5, "bizepsLinks": 37.0, "bizepsRechts": 38.3, "unterarmLinks": 31.7, "unterarmRechts": 33.6 },
-        { "datum": "2026-08-18", "brust": 116.8, "schulter": 128.5, "bauch": 113.2, "taille": 103.0, "oberschenkelLinks": 65.9, "oberschenkelRechts": 64.7, "wadeLinks": 46.5, "wadeRechts": 45.0, "bizepsLinks": 36.8, "bizepsRechts": 39.4, "unterarmLinks": 31.6, "unterarmRechts": 33.4 }
+        { "datum": "2026-08-18", "brust": 116.8, "schulter": 128.5, "bauch": 113.2, "taille": 103.0, "oberschenkelLinks": 65.9, "oberschenkelRechts": 64.7, "wadeLinks": 46.5, "wadeRechts": 45.0, "bizepsLinks": 36.8, "bizepsRechts": 39.4, "unterarmLinks": 31.6, "unterarmRechts": 33.4 },
+        { "datum": "2026-09-17", "brust": 116.2, "schulter": 127.8, "bauch": 114.8, "taille": 105.2, "oberschenkelLinks": 63.1, "oberschenkelRechts": 64.8, "wadeLinks": 45.9, "wadeRechts": 45.2, "bizepsLinks": 36.3, "bizepsRechts": 38.3, "unterarmLinks": 31.7, "unterarmRechts": 33.9 }
       ]
     },
     {
@@ -30,11 +36,17 @@ window.__WEIGHT_DATA__ = {
         { "datum": "2026-07-26", "gewicht": 84.8 },
         { "datum": "2026-08-02", "gewicht": 85.9 },
         { "datum": "2026-08-10", "gewicht": 85.0 },
-        { "datum": "2026-08-18", "gewicht": 86.4, "kfa": 23.2, "muskelmasse": 63.07, "ffm": 66.36 }
+        { "datum": "2026-08-18", "gewicht": 86.4, "kfa": 23.2, "muskelmasse": 63.07, "ffm": 66.36 },
+        { "datum": "2026-08-23", "gewicht": 86.9 },
+        { "datum": "2026-08-31", "gewicht": 86.7 },
+        { "datum": "2026-09-07", "gewicht": 86.8 },
+        { "datum": "2026-09-13", "gewicht": 88.2 },
+        { "datum": "2026-09-18", "gewicht": 89.1, "kfa": 21.2, "muskelmasse": 63.94, "ffm": 67.32 },
       ],
       "umfaenge": [
         { "datum": "2026-07-13", "brust": 96.5, "schulter": 119.0, "bauch": 85.4, "taille": 82.1, "oberschenkelLinks": 53.2, "oberschenkelRechts": 52.0, "wadeLinks": 38.6, "wadeRechts": 37.4, "bizepsLinks": 31.8, "bizepsRechts": 32.7, "unterarmLinks": 28.4, "unterarmRechts": 29.1 },
-        { "datum": "2026-08-18", "brust": 99.6, "schulter": 121.1, "bauch": 89.4, "taille": 84.3, "oberschenkelLinks": 55.2, "oberschenkelRechts": 54.2, "wadeLinks": 39.6, "wadeRechts": 38.3, "bizepsLinks": 33.0, "bizepsRechts": 33.1, "unterarmLinks": 29.1, "unterarmRechts": 29.8 }
+        { "datum": "2026-08-18", "brust": 99.6, "schulter": 121.1, "bauch": 89.4, "taille": 84.3, "oberschenkelLinks": 55.2, "oberschenkelRechts": 54.2, "wadeLinks": 39.6, "wadeRechts": 38.3, "bizepsLinks": 33.0, "bizepsRechts": 33.1, "unterarmLinks": 29.1, "unterarmRechts": 29.8 },
+        { "datum": "2026-09-17", "brust": 103.9, "schulter": 120.4, "bauch": 91.5, "taille": 86.3, "oberschenkelLinks": 55.1, "oberschenkelRechts": 56.8, "wadeLinks": 40.5, "wadeRechts": 39.4, "bizepsLinks": 32.2, "bizepsRechts": 32.6, "unterarmLinks": 29.1, "unterarmRechts": 29.8 }
       ]
     }
   ]
